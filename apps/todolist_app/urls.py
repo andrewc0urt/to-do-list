7fd30b9be76b057e7ebe27ff0e8beb9870814cbf
andrewc0urt <andrew.court.dev@gmail.com>
2023-09-15 20:26:54 -0400

    Added User Signup and Login Forms, Views, and URL Paths
    Create a user signup form in signup.html.
    
    Create a login form in user_login.html.
    
    Set up view methods in 'views.py' for user signup, login, and logout, focusing only on rendering the appropriate HTML pages for now.
    
    Configured URL paths for the signup, login, and logout functionality in 'urls.py'.

--- apps/todolist_app/urls.py
+++ apps/todolist_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('signup/', views.user_signup, name='user_signup' ),
+    path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout')
 ]
--- apps/todolist_app/views.py
+++ apps/todolist_app/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html", {})
+
+
+def user_signup(request):
+    return render(request, "signup.html", {})
+
+def user_login(request):
+    return render(request, "user_login.html", {})
+
+def user_logout(request):
+    return render(request, "user_login.html")
