ddf149808a2c994f61417c720954b3f5205def6c
andrewc0urt <andrew.court.dev@gmail.com>
2023-10-06 15:49:58 -0400

    Registered Models in Django Admin
    Registered ToDoList, ItemsInToDoList, Date, and CustomUser models in the admin.py file for easy management and access via the Django Admin dashboard.

--- apps/todolist_app/admin.py
+++ apps/todolist_app/admin.py
@@ -1,3 +1,14 @@
+# admin.py - Configuration for Django Admin Interface
+
+# This file defines how the models in this app are displayed and managed
+# through the Django Admin Interface. It includes model registration,
+# customization, permissions, and more for admin users.
+
 from django.contrib import admin
+from .models import ToDoList, ItemsInToDoList, Date, CustomUser
 
 # Register your models here.
+admin.site.register(ToDoList)
+admin.site.register(ItemsInToDoList)
+admin.site.register(Date)
+admin.site.register(CustomUser)
