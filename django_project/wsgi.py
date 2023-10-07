d1760a855755d438ed23be4d915b3a3f5a2487e2
andrewc0urt <andrew.court.dev@gmail.com>
2023-10-07 00:24:48 -0400

    Added Comment to wsgi.py

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 application = get_wsgi_application()
+
+# May need to edit this page due to Heroku deployment issues
